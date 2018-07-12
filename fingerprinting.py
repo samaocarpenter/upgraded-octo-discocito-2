beceeb85c0ec9e608e96836b433661dc1ecf862e
BrokenToaster1 <aleksey.didenkov@gmail.com>
2018-07-12 16:21:51 -0400

    Added add_to_dict

--- fingerprinting.py
+++ fingerprinting.py
@@ -2,6 +2,8 @@ import numpy as np
 
 def fingerprints(specgram, fanout=5):
     '''
+    Analyzes a peaks spectrogram and extracts pairwise fingerprints
+
     INPUTS
 
         specgram:   spectrogram to analyze
@@ -26,3 +28,27 @@ def fingerprints(specgram, fanout=5):
         for j in range(i+1, min(i+1+fanout, len(t))):
             out = np.append(out, [[(f[i], f[j], t[j]-t[i]), t[i]]], axis=0)
     return out[1:, :]
+
+def add_to_dict(diction, fings, song_id):
+    '''
+    Add results of fingerprints() to an existing (or empty) dictionary
+
+    INPUTS
+
+        diction:    dictionary to modify
+        
+        fings:      output from fingerprints()
+
+        song_id:    id to catalogue the song as
+
+    OUTPUTS
+
+        out:        same dictionary as diction
+    '''
+    for fing in fings:
+        tup = (song_id, fing[1])
+        if(fing[0] in diction):
+            diction[fing[0]].append(tup)
+        else:
+            diction[fing[0]] = [tup]
+    return diction
