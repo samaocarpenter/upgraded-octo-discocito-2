49a5b19ad2cfcc71f80254018f37932307b30528
Sam Carpenter <HardBoiled800@users.noreply.github.com>
2018-07-13 14:53:40 -0400

    Update song_to_fingerprint.py

--- song_to_fingerprint.py
+++ song_to_fingerprint.py
@@ -3,7 +3,7 @@ import fingerprinting
 from spectrogram_to_cutoff_and_peaks import *
 
 def song_to_fingerprint(sample):
-    S = Spectrogram.spectrogram(sample)
+    S = Spectrogram.create_spectrogram(sample)
     cutoff = spectrogram_to_cutoff(S)
     peaks = spectrogram_to_peaks(S, cutoff)
     return fingerprinting.fingerprints(peaks)
