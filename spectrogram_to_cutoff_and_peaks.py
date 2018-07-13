0e5711493279dd6604254378f83e43eb9d3c800e
Sam Carpenter <HardBoiled800@users.noreply.github.com>
2018-07-12 20:24:34 -0400

    Update spectrogram_to_cutoff_and_peaks.py

--- spectrogram_to_cutoff_and_peaks.py
+++ spectrogram_to_cutoff_and_peaks.py
@@ -22,7 +22,7 @@ def spectrogram_to_cutoff(spectrogram, frac_cut = 0.9):
         The cutoff value used to distinguish the foreground from the 
         background. """
     
-    flat_spec[flat_spec <= 0] = 0.00001
+    flat_spec[flat_spec <= 0] = 1e-8
     
     flat_spec = np.log(spectrogram.flatten())
     N = len(flat_spec)
