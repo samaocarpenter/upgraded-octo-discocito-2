cb92cb2b403da405d9c1538aa59c3eeb6b30a04f
Christina Xiao <christinaxiao@Christinas-MacBook-Air.local>
2018-07-13 13:31:03 -0400

    changed default values for iterations

--- spectrogram_to_cutoff_and_peaks.py
+++ spectrogram_to_cutoff_and_peaks.py
@@ -14,23 +14,24 @@ def spectrogram_to_cutoff(spectrogram, frac_cut = 0.9):
             2D array of shape (N, M)
             
         frac_cut : int
-            the percentage of lower amplitudes that wull be considered 
+            the percentage of lower amplitudes that will be considered 
             background
             
         Returns
         -------
         The cutoff value used to distinguish the foreground from the 
         background. """
-    
-    spectrogram[spectrogram <= 0] = 1e-15
+        
+    #spectrogram[spectrogram <= 0] = 1e-15
+    spectrogram += 1e-20
     
     flat_spec = np.log(spectrogram.flatten())
     N = len(flat_spec)
-
-    hist, bin_edges = np.histogram(flat_spec, bins = int(N / 2), density = True)
+    
+    bin_counts, bin_edges = np.histogram(flat_spec, bins = int(N / 2), density = True)
     bin_size = np.diff(bin_edges)
     
-    cumulative_distr = np.cumsum(hist * bin_size)
+    cumulative_distr = np.cumsum(bin_counts * bin_size)
     
     bin_index_of_cutoff = np.searchsorted(cumulative_distr, frac_cut)
     
@@ -38,7 +39,7 @@ def spectrogram_to_cutoff(spectrogram, frac_cut = 0.9):
     cutoff = bin_edges[bin_index_of_cutoff]
     return cutoff
 
-def spectrogram_to_peaks(spectrogram, cutoff):
+def spectrogram_to_peaks(spectrogram, cutoff, iterations = 3):
     """ Given a spectrogram, return its peaks, which are all local peaks
         with values greater than the cutoff. 
         
@@ -51,11 +52,13 @@ def spectrogram_to_peaks(spectrogram, cutoff):
             elements less than or equal to the cutoff are considered 
             background elements
             
+        iterations : int
+            the number of iterations for the iterated footprint
+            
         Returns
         -------
         A bool array of the peaks of a spectrogram. """
     
-    # Ryan's neighborhood
     fp = generate_binary_structure(rank = 2, connectivity = 1)
-    spec_max = maximum_filter(spectrogram, footprint = iterate_structure(fp, 20))
+    spec_max = maximum_filter(spectrogram, footprint = iterate_structure(fp, iterations))
     return (spectrogram == spec_max) & (spectrogram > cutoff)
